1f14eb377049b3b55e65257d0a11a3dad4865b36
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-01-06 16:36:47 -0500

    Added handling for groupme

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ client.on("ready", async () => {
   // const channel = guild.channels.cache.get("894349711761346612");
 });
 
-app.get("/", async (req, res) => {
-  if (channel) channel.send("This message came from an HTTP request.");
-  else console.log("No channel found.");
-  res.send("<h1>Hello World!</h1>");
+app.post("/groupme", async (req, res) => {
+  console.log(req);
+  // if (channel) channel.send("This message came from an HTTP request.");
+  // else console.log("No channel found.");
+  // res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
