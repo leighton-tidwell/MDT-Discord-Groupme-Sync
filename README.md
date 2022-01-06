744aeff10a71c4032dd8a6b5a58816cf4b78e811
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-01-06 16:13:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MDT Groupme Discord Sync
+
+Simple bot that syncs groupme messages to a discord server.
---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config(); //initialize dotenv
+const { DISCORD_TOKEN } = process.env;
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  const guild = await client.guilds.fetch("928711880552366082");
+  const channel = guild.channels.cache.get("928741601658933298");
+  // channel.send("I'm online!");
+  // const channel = guild.channels.cache.get("894349711761346612");
+});
+
+client.login(DISCORD_TOKEN);
