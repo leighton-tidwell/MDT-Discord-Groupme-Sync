791ad8bddd969fa8146ecc9e093bfe12d6ba5a70
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-01-06 16:25:20 -0500

    Testing express server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config(); //initialize dotenv
 const { DISCORD_TOKEN } = process.env;
 const { Client, Intents } = require("discord.js");
-const http = require("http");
+const express = require("express");
+const app = express();
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS],
@@ -14,5 +15,11 @@ client.on("ready", async () => {
   // channel.send("I'm online!");
   // const channel = guild.channels.cache.get("894349711761346612");
 });
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
 
 client.login(DISCORD_TOKEN);
